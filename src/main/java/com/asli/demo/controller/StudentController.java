aef78775de7c28be6948a384171bbc5948f7bf12
Aslı Nur Mavili <aslinurmavili@gmail.com>
2025-08-31 15:35:19 +0300

    "Lazy vs Eager Fetch ve Sonsuz Döngü "

--- src/main/java/com/asli/demo/controller/StudentController.java
+++ src/main/java/com/asli/demo/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.asli.demo.controller;
 import com.asli.demo.entity.Student;
 import com.asli.demo.service.StudentService;
 import org.springframework.web.bind.annotation.*;
+import com.asli.demo.entity.Book;
 
 import java.util.List;
 
@@ -16,18 +17,27 @@ public class StudentController {
         this.studentService = studentService;
     }
 
+
     @PostMapping
     public Student createStudent(@RequestBody Student student) {
         return studentService.saveStudent(student);
     }
+
 
     @GetMapping
     public List<Student> getAllStudents() {
         return studentService.getAllStudents();
     }
 
+
     @DeleteMapping("/{id}")
     public void deleteStudent(@PathVariable Long id) {
         studentService.deleteStudent(id);
+    }
+
+
+    @PostMapping("/{studentId}/books")
+    public Student addBookToStudent(@PathVariable Long studentId, @RequestBody Book book) {
+        return studentService.addBookToStudent(studentId, book);
     }
 }
--- src/main/java/com/asli/demo/service/StudentService.java
+++ src/main/java/com/asli/demo/service/StudentService.java
@@ -1,10 +1,12 @@
 package com.asli.demo.service;
 
+import com.asli.demo.entity.Book;
 import com.asli.demo.entity.Student;
 import com.asli.demo.repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -28,5 +30,17 @@ public class StudentService {
 
     public void deleteStudent(Long id) {
         studentRepository.deleteById(id);
+    }
+
+
+    public Student addBookToStudent(Long studentId, Book book) {
+        Optional<Student> optionalStudent = studentRepository.findById(studentId);
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.get();
+            student.addBook(book);
+            return studentRepository.save(student);
+        } else {
+            throw new RuntimeException("Öğrenci bulunamadı, ID: " + studentId);
+        }
     }
 }
